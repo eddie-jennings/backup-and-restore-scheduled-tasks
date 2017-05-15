5684d3491869c54261360ec010569ce2d3bc459f
Eddie Jennings, Jr <eddie@eddiejennings.net>
2017-05-15 15:47:29 -0400

    Export scheduled tasks

---
+++ TaskBackup.ps1
@@ -0,0 +1,10 @@
+﻿#Run with Administrator privileges
+
+
+$taskStoragePath = "\NameOfPathInTaskScheduler\" # \ is the root of the Task Scheduler
+$taskBackupPath = "C:\PathToWhereXMLFileWillLive\"
+$tasks = Get-ScheduledTask -TaskPath $taskStoragePath
+
+foreach ($tasks in $tasks) { Export-ScheduledTask -TaskName $tasks.TaskName -TaskPath $taskStoragePath | out-file -FilePath $($taskBackupPath + $tasks.TaskName + ".xml") }
+
+(Get-ScheduledTask -TaskPath $taskStoragePath) | Export-Csv $($taskBackupPath + "alltasks.csv") #Used in the restore script for Task Names
